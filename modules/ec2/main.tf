8746b972f4c3dcd4fc47d90eeee364cb33498631
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-23 18:09:52 +0530

    teraform code

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instances" {
   ami                    = var.ami_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = var.vpc_security_group-ids
+  vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
     Name = var.tag_name
@@ -13,5 +13,5 @@ resource "aws_route53_record" "route53_records" {
   zone_id = var.zone_id
   name    = "${var.tag_name}-${var.env}"
   type    = "A"
-  records = aws_instance.instances.private_ip
+  records = [aws_instance.instances.private_ip]
 }
--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -2,5 +2,5 @@ variable "ami_id" {}
 variable "tag_name" {}
 variable "instance_type" {}
 variable "zone_id" {}
-variable "vpc_security_group-ids" {}
+variable "vpc_security_group_ids" {}
 variable "env" {}
