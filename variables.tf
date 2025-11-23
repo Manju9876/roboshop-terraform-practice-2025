f93467b7bf66cc41349b93fa4d88011badc98c4e
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-23 18:15:30 +0530

    teraform code

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 # variable "ami_id" {}
-# variable "tag_name" {}
-variable "instance_type" {}
+variable "tag_name" {}
+# variable "instance_type" {}
 variable "zone_id" {}
 variable "env" {}
 variable "vpc_security_group_ids" {}
