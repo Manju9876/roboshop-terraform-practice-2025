70d9449ed5e70cf19c7eafacf65b9b96b9ed88a5
Manju9876 <129134583+Manju9876@users.noreply.github.com>
2025-11-23 18:42:51 +0530

    teraform code

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -8,11 +8,11 @@ resource "aws_instance" "instances" {
   }
 }
 
-
-resource "aws_route53_record" "route53_records" {
-  zone_id = var.zone_id
-  name    = "${var.tag_name}-${var.env}"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instances.private_ip]
-}
+#
+# resource "aws_route53_record" "route53_records" {
+#   zone_id = var.zone_id
+#   name    = "${var.tag_name}-${var.env}"
+#   type    = "A"
+#   ttl     = 30
+#   records = [aws_instance.instances.private_ip]
+# }
