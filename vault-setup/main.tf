198ea55338a0ce97703b9b51a9658da8df011fdd
Manju <manjunath960650@gmail.com>
2025-12-03 12:26:09 +0530

    terraform practice

--- vault-setup/main.tf
+++ vault-setup/main.tf
@@ -1,3 +1,4 @@
+
 provider "vault" {
    address = "http://vault.devopsbymanju.shop:8200"
   token = var.vault_token
@@ -9,3 +10,9 @@ data "vault_generic_secret" "sample" {
   path = "test/demo"
 }
 
+resource "local_file" "sample" {
+  filename = "/tmp/demo.txt"
+  content = <<EOF
+username: ${data.vault_generic_secret.data["user_name"]}
+EOF
+}
